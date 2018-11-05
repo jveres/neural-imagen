fdcc59a75143054b0e7e9324035dc6f40363091a
jveres <janos.veres@gmail.com>
2018-11-05 11:44:58 +0100

    Update resize.py

--- imagen/data/resize.py
+++ imagen/data/resize.py
@@ -12,8 +12,8 @@ class DownscaleBuilder:
                             kernel_size=(factor, factor), stride=(factor, factor))
         )
 
-        self.downscaler[0].bias[:] = 0.0
-        self.downscaler[0].weight[:,0,:,:] = 1.0 / (factor ** 2)
+        #self.downscaler[0].bias[:] = 0.0
+        #self.downscaler[0].weight[:,0,:,:] = 1.0 / (factor ** 2)
 
     def build(self, image):
         return self.downscaler(image)
